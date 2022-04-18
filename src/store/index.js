8b969b5ff9cd7d1646d4d55523e88e64dabe7713
AlekseevaMargarita <amv1102@mail.ru>
2022-04-18 23:32:12 +0300

    Выполнено задание 4

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,17 @@
 import { combineReducers, createStore, applyMiddleware, compose } from "redux";
+import persistReducer from "redux-persist/es/persistReducer";
+import persistStore from "redux-persist/es/persistStore";
+import storage from "redux-persist/lib/storage";
 import thunk from 'redux-thunk';
 
 import { chatsReducer } from "./chats/reducer";
 import { messagesReducer } from "./messages/reducer";
 import { profileReducer } from "./profile/reducer";
+
+const persistConfig = {
+    key: 'myapp',
+    storage,
+};
 
 const rootReducer = combineReducers({
     profile: profileReducer,
@@ -11,9 +19,13 @@ const rootReducer = combineReducers({
     messages: messagesReducer,
 });
 
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export const store = createStore(
-    rootReducer,
+    persistedReducer,
     composeEnhancers(applyMiddleware(thunk)),
 );
+
+export const persistor = persistStore(store);
