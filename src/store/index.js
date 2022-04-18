0e0faef53a270daeed86c268d1b710f2464b19dd
AlekseevaMargarita <amv1102@mail.ru>
2022-04-18 21:23:42 +0300

    Выполнено задание 1

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,19 @@
-import { combineReducers, createStore } from "redux";
+import { combineReducers, createStore, applyMiddleware, compose } from "redux";
+import thunk from 'redux-thunk';
+
 import { chatsReducer } from "./chats/reducer";
 import { messagesReducer } from "./messages/reducer";
 import { profileReducer } from "./profile/reducer";
 
+const rootReducer = combineReducers({
+    profile: profileReducer,
+    chats: chatsReducer,
+    messages: messagesReducer,
+});
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export const store = createStore(
-    combineReducers({
-        profile: profileReducer,
-        chats: chatsReducer,
-        messages: messagesReducer,
-    }),
-    window.__REDUX_DEVTOOLS_EXTENSION__ &&
-    window.__REDUX_DEVTOOLS_EXTENSION__()
+    rootReducer,
+    composeEnhancers(applyMiddleware(thunk)),
 );
