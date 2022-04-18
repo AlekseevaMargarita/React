1eb83db315a1137657869ec2aaba2825737f51fb
AlekseevaMargarita <amv1102@mail.ru>
2022-04-19 00:59:24 +0300

    Выполнено задание 5

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,13 @@
-import { combineReducers, createStore, applyMiddleware, compose } from "redux";
+import { combineReducers, createStore, applyMiddleware } from "redux";
 import persistReducer from "redux-persist/es/persistReducer";
 import persistStore from "redux-persist/es/persistStore";
 import storage from "redux-persist/lib/storage";
-import thunk from 'redux-thunk';
+import createSagaMiddleware from "@redux-saga/core";
 
 import { chatsReducer } from "./chats/reducer";
 import { messagesReducer } from "./messages/reducer";
 import { profileReducer } from "./profile/reducer";
+import mySaga from "./messages/sagas";
 
 const persistConfig = {
     key: 'myapp',
@@ -21,11 +22,13 @@ const rootReducer = combineReducers({
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const sagaMiddleware = createSagaMiddleware();
 
 export const store = createStore(
     persistedReducer,
-    composeEnhancers(applyMiddleware(thunk)),
+    applyMiddleware(sagaMiddleware),
 );
 
 export const persistor = persistStore(store);
+
+sagaMiddleware.run(mySaga);
