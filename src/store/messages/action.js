e7863a00b026c2d58e82d652d3901df32c366a07
AlekseevaMargarita <amv1102@mail.ru>
2022-04-18 23:04:38 +0300

    Выполнено задание 2

--- src/store/messages/action.js
+++ src/store/messages/action.js
@@ -1,3 +1,4 @@
+import { AUTHORS } from "../../constants/constants";
 
 export const ADD_MESSAGE = 'MESSAGES::ADD_MESSAGE';
 
@@ -8,3 +9,28 @@ export const addMessage = (chatId, message) => ({
         message,
     },
 });
+
+let timerId;
+
+export const addMessageWithThunk = (chatId, message) => (dispatch) => {
+    dispatch(addMessage(chatId, message));
+
+    if (timerId) {
+        clearTimeout(timerId)
+    }
+
+    if (message.author !== AUTHORS.BOT) {
+        timerId = setTimeout(() => {
+            dispatch(
+                addMessage(
+                    chatId,
+                    {
+                        messageId: `id${Date.now()}`,
+                        text: 'Ваше сообщение прочитано',
+                        author: AUTHORS.BOT,
+                    }
+                )
+            )
+        }, 1500);
+    }
+};
