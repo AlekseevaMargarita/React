68581e7c6d889193e3ab3bb8d24a9af8bceb3ca4
AlekseevaMargarita <amv1102@mail.ru>
2022-05-28 23:42:32 +0300

    Внесены изменения по замечаниям

--- src/store/facts/__tests__/action.test.js
+++ src/store/facts/__tests__/action.test.js
@@ -1,13 +1,13 @@
 import { getFacts, getFactsFailure, getFactsRequest, getFactsSuccess } from '../action';
 
 describe('getFacts tests', () => {
-    it('--- dispatches getFactsRequest', () => {
+    it('dispatches getFactsRequest', () => {
         const mockDispatch = jest.fn();
         getFacts()(mockDispatch);
         expect(mockDispatch).toHaveBeenCalledWith(getFactsRequest());
     });
 
-    it('--- calls getFactsSuccess if fetch was succesfull', async () => {
+    it('calls getFactsSuccess if fetch was succesfull', async () => {
         const mockDispatch = jest.fn();
         const body = { test: 'test' };
         fetch.mockResponseOnce(JSON.stringify(body));
@@ -15,7 +15,7 @@ describe('getFacts tests', () => {
         expect(mockDispatch).toHaveBeenLastCalledWith(getFactsSuccess(body));
     });
 
-    it('--- calls getFactsFailure if fetch was unsuccesfull', async () => {
+    it('calls getFactsFailure if fetch was unsuccesfull', async () => {
         const mockDispatch = jest.fn();
         const error = { message: 'test' };
         fetch.mockRejectOnce(error);
--- src/store/facts/__tests__/reducer.test.js
+++ src/store/facts/__tests__/reducer.test.js
@@ -2,17 +2,17 @@ import { getFactsFailure, getFactsRequest, getFactsSuccess } from '../action';
 import { factsReducer, initialState } from '../reducer';
 
 describe('factsReducer tests with snapshot', () => {
-    it('--- return correct state after GET_FACTS_REQUEST action', () => {
+    it('return correct state after GET_FACTS_REQUEST action', () => {
         const factStore = factsReducer(initialState, getFactsRequest());
         expect(factStore).toMatchSnapshot();
     });
 
-    it('--- return correct state after GET_FACTS_SUCCESS action', () => {
+    it('return correct state after GET_FACTS_SUCCESS action', () => {
         const factStore = factsReducer(initialState, getFactsSuccess());
         expect(factStore).toMatchSnapshot();
     });
 
-    it('--- return correct state after GET_FACTS_FAILURE action', () => {
+    it('return correct state after GET_FACTS_FAILURE action', () => {
         const factStore = factsReducer(initialState, getFactsFailure());
         expect(factStore).toMatchSnapshot();
     });
